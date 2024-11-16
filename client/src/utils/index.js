83f73acae313aec9152478a3a005994e16566300
hmimed70 <41494431+hmimed70@users.noreply.github.com>
2024-11-16 11:05:59 +0100

    adding forntend and backend URL

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
-export const BACKEND_URL=  "http://localhost:8000" 
-//export const BACKEND_URL = "https://orderly-9j1s.onrender.com"
+//export const BACKEND_URL=  "http://localhost:8000" 
+export const BACKEND_URL = "https://orderly-9j1s.onrender.com"
 
 // Assuming `user.createdAt` is the date field from MongoDB
 import wilayas from '../data/Wilaya.json';
--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', //process.env.FRONTEND_URL, // frontend URL
+  origin: process.env.FRONTEND_URL, // frontend URL
   credentials: true,
 }));
 app.options('*', cors());
